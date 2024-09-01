df4d72441cd242bcd0671ef3cd5d498405bd14fd
1rajlaxmi <132902555+1rajlaxmi@users.noreply.github.com>
2024-08-31 18:57:27 -0700

    Update auth_controller.js 2 check

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -9,44 +9,44 @@ export const signup = async (req, res) => {
             return res.status(400).json({ e: "Password don't match" });
         }
 
-        const user = await User.findOne({ username });
-        if (user) {
-            return res.status(400).json({ e: "User name already exits" })
-        }
-	 return res.status(400).json({ e: fullname });
-  //       //Hash password
-  //       const salt = await bcrypt.genSalt(10);
-		// const hashedPassword = await bcrypt.hash(password, salt);
+        // const user = await User.findOne({ username });
+        // if (user) {
+        //     return res.status(400).json({ e: "User name already exits" })
+        // }
+	
+        //Hash password
+        const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
 
 
-  //       // https://avatar-placeholder.iran.liara.run/
+        // https://avatar-placeholder.iran.liara.run/
 
-  //       const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-  //       const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-  //       const newUser = new User({
-  //           fullname,
-  //           username,
-  //           password :hashedPassword,
-  //           gender,
-  //           profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
-  //       });
+        const newUser = new User({
+            fullname,
+            username,
+            password :hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
 
-  //       if (newUser) {
-  //           // Generate JWT token here
-		// 	generateTokenAndSetCookie(newUser._id, res);
-  //           await newUser.save();
+        if (newUser) {
+            // Generate JWT token here
+			generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
 
-  //           res.status(201).json({
-  //               _id: newUser._id,
-  //               fullname: newUser.fullname,
-  //               username: newUser.username,
-  //               profilePic: newUser.profilePic,
-  //           });
-  //       } 
-  //    else {
-  //       res.status(400).json({ error: "Invalid user data" });
-  //   }
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        } 
+     else {
+        res.status(400).json({ error: "Invalid user data" });
+    }
 } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
